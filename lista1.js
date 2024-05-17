348af96c789d33582c5e89ec30c4a340d5b0f3ba
Fidogreg <rafael.moretti@escola.pr.gov.br>
2024-05-17 12:38:05 -0300

    feat add exercicios

---
+++ lista1.js
@@ -0,0 +1,10 @@
+// Tipo number
+
+const numerointeiro = 30;
+const numerointeiro1 = 70;
+
+const soma = numerointeiro + numerointeiro1
+
+// Mostrar as operacoes
+
+console.log(soma);
---
+++ lista2.js
@@ -0,0 +1,9 @@
+// Tipo number
+
+const numero = 400;
+const frase = "Valor da Variavel ";
+const soma = frase + numero;
+
+// Mostrar a operacao
+
+console.log(soma);
